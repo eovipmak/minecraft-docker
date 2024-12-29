c6ad205d821f6778c09a5c7610e6f1b34472ef02
Tran Nguyen Tin <62509465+eovipmak@users.noreply.github.com>
2024-12-29 17:04:09 +0700

    rewrite

--- purpur-java 21/genarate-config.sh
+++ purpur-java 21/genarate-config.sh
@@ -1,36 +1,53 @@
 #!/bin/bash
+
+# Fetch the latest version
+get_latest_version() {
+    curl -s https://api.purpurmc.org/v2/${PROJECT} | jq -r '.versions[-1]'
+}
+
+# Check if a version exists
+version_exists() {
+    curl -s https://api.purpurmc.org/v2/${PROJECT} | jq -r --arg VERSION "$1" '.versions[] | contains($VERSION)' | grep -q true
+}
+
+# Fetch the latest build for a version
+get_latest_build() {
+    curl -s https://api.purpurmc.org/v2/${PROJECT}/${1} | jq -r '.builds.latest'
+}
+
+# Check if a build exists for a version
+build_exists() {
+    curl -s https://api.purpurmc.org/v2/${PROJECT}/${1} | jq -r --arg BUILD "$2" '.builds.all | tostring | contains($BUILD)' | grep -q true
+}
+
+
 MINECRAFT_VERSION="${1:-latest}"
 BUILD_NUMBER="${2:-latest}"
-
 PROJECT=purpur
 
-VER_EXISTS=`curl -s https://api.purpurmc.org/v2/${PROJECT} | jq -r --arg VERSION $MINECRAFT_VERSION '.versions[] | contains($VERSION)' | grep true`
-LATEST_VERSION=`curl -s https://api.purpurmc.org/v2/${PROJECT} | jq -r '.versions' | jq -r '.[-1]'`
+# Check if the specified Minecraft version exists
+if version_exists "$MINECRAFT_VERSION"; then
+    echo "Version is valid. Using version ${MINECRAFT_VERSION}"
+else
+    echo "Using the latest ${PROJECT} version"
+    MINECRAFT_VERSION=$(get_latest_version)
+fi
 
-#Check if MINECRAFT_VERSION is valid, otherwise use latest version
-if [ "${VER_EXISTS}" == "true" ]; then
-		echo -e "Version is valid. Using version ${MINECRAFT_VERSION}"
-	else
-		echo -e "Using the latest ${PROJECT} version"
-		MINECRAFT_VERSION=${LATEST_VERSION}
-	fi
+# Check if the specified build number exists for minecraft version
+if build_exists "$MINECRAFT_VERSION" "$BUILD_NUMBER"; then
+    echo "Build is valid for version ${MINECRAFT_VERSION}. Using build ${BUILD_NUMBER}"
+else
+    echo "Using the latest ${PROJECT} build for version ${MINECRAFT_VERSION}"
+    BUILD_NUMBER=$(get_latest_build "$MINECRAFT_VERSION")
+fi
 
-BUILD_EXISTS=`curl -s https://api.purpurmc.org/v2/${PROJECT}/${MINECRAFT_VERSION} | jq -r --arg BUILD ${BUILD_NUMBER} '.builds.all | tostring | contains($BUILD)' | grep true`
-LATEST_BUILD=`curl -s https://api.purpurmc.org/v2/${PROJECT}/${MINECRAFT_VERSION} | jq -r '.builds.latest'`
+# Export version and build number
+export MINECRAFT_VERSION
+export BUILD_NUMBER
 
-#Check if BUILD NUMBER is valid, otherwise use latest build
-if [ "${BUILD_EXISTS}" == "true" ]; then
-		echo -e "Build is valid for version ${MINECRAFT_VERSION}. Using build ${BUILD_NUMBER}"
-	else
-		echo -e "Using the latest ${PROJECT} build for version ${MINECRAFT_VERSION}"
-		BUILD_NUMBER=${LATEST_BUILD}
-	fi
-
-export MINECRAFT_VERSION=$MINECRAFT_VERSION
-export BUILD_NUMBER=$BUILD_NUMBER
-
-sed -i "s/MINECRAFT_VERSION: \"1.21.3\"/MINECRAFT_VERSION: \"$MINECRAFT_VERSION\"/" docker-compose.yml
-echo -e "Setup MINECRAFT_VERSION=$MINECRAFT_VERSION in docker-compose.yml"
-sed -i "s/BUILD_NUMBER: \"2358\"/BUILD_NUMBER: \"$BUILD_NUMBER\"/" docker-compose.yml
-echo -e "Setup BUILD_NUMBER=$BUILD_NUMBER in docker-compose.yml"
+# Update the value in docker-compose.yml
+sed -i "s/MINECRAFT_VERSION: \"[^\"]*\"/MINECRAFT_VERSION: \"$MINECRAFT_VERSION\"/" docker-compose.yml
+echo "Setup MINECRAFT_VERSION=$MINECRAFT_VERSION in docker-compose.yml"
+sed -i "s/BUILD_NUMBER: \"[^\"]*\"/BUILD_NUMBER: \"$BUILD_NUMBER\"/" docker-compose.yml
+echo "Setup BUILD_NUMBER=$BUILD_NUMBER in docker-compose.yml"
 
